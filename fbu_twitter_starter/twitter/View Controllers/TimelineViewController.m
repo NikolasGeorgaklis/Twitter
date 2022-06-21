6457cc7738f748b225e13ffe0270a6ee6dca9a24
Nikolas Georgaklis <nikog@fb.com>
2022-06-21 16:10:05 -0700

    Implemented user logout functionality

--- fbu_twitter_starter/twitter/View Controllers/TimelineViewController.m
+++ fbu_twitter_starter/twitter/View Controllers/TimelineViewController.m
@@ -8,12 +8,24 @@
 
 #import "TimelineViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController ()
 
 @end
 
 @implementation TimelineViewController
+- (IBAction)didTapLogout:(id)sender {
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    
+    //clear out access tokens
+    [[APIManager shared] logout];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
